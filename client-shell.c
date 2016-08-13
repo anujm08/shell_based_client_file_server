94d285c38a3152113d43cfb5bce58c749dde1f24
anuj <anujmittal0801@gmail.com>
2016-08-13 22:27:26 +0530

    Added basic shell running basic linux commands

---
+++ client-shell.c
@@ -0,0 +1,99 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+char **tokenize(char *line)
+{
+	char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+	char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+	int i, tokenIndex = 0, tokenNo = 0;
+
+	for(i =0; i < strlen(line); i++)
+	{
+		char readChar = line[i];
+
+		if (readChar == ' ' || readChar == '\n' || readChar == '\t')
+		{
+			token[tokenIndex] = '\0';
+			if (tokenIndex != 0)
+			{
+				tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
+				strcpy(tokens[tokenNo++], token);
+				tokenIndex = 0; 
+			}
+		} 
+		else
+		{
+			token[tokenIndex++] = readChar;
+		}
+	}
+ 
+	free(token);
+	tokens[tokenNo] = NULL ;
+	return tokens;
+}
+
+
+int  main(void)
+{
+	char  line[MAX_INPUT_SIZE];            
+	char  **tokens;              
+	int i;
+
+	while (1)
+	{           
+	   
+		printf("Hello>");     
+		bzero(line, MAX_INPUT_SIZE);
+		gets(line);           
+		printf("Got command %s\n", line);
+		line[strlen(line)] = '\n'; //terminate with new line
+		tokens = tokenize(line);
+
+		//do whatever you want with the commands, here we just print them
+
+		pid_t pid;
+		pid = fork();
+
+		if (pid < 0)
+		{
+        	perror("ERROR: forking child process failed\n");
+     		continue;
+     	}
+     	else if (pid == 0)
+        {
+          	if (execvp(*tokens, tokens) < 0)
+          	{
+               printf("ERROR: exec failed\n");
+               continue;
+          	}
+     	}
+     	else 
+     	{
+          	waitpid(pid, NULL, 0);
+     	}
+
+		// Freeing the allocated memory	
+		for(i=0;tokens[i]!=NULL;i++)
+		{
+			free(tokens[i]);
+		}
+		free(tokens);
+
+	}	
+	return 0;
+}
+
+				
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all: client server
+all: client-shell client server
 
 client: get-one-file.c get-one-file-sig.c
 	gcc -w -o get-one-file get-one-file.c
@@ -7,6 +7,11 @@ client: get-one-file.c get-one-file-sig.c
 server: server-slow.c
 	gcc -w -o server-slow server-slow.c -lpthread
 
+client-shell: client-shell.c
+	gcc -w -o client-shell client-shell.c
+
 clean: 
 	rm get-one-file
+	rm get-one-file-sig
 	rm server-slow
+	rm client-shell
